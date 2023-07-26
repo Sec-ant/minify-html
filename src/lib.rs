f2501e3a0714c1347919b05153a201d27351d397
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-07-26 12:12:39 +0800

    fix: make config properties optional

--- src/lib.rs
+++ src/lib.rs
@@ -10,38 +10,39 @@ static ALLOCATOR: LockedAllocator<FreeListAllocator> =
 
 #[wasm_bindgen]
 pub struct Config {
-    pub do_not_minify_doctype: bool,
-    pub ensure_spec_compliant_unquoted_attribute_values: bool,
-    pub keep_closing_tags: bool,
-    pub keep_html_and_head_opening_tags: bool,
-    pub keep_spaces_between_attributes: bool,
-    pub keep_comments: bool,
-    pub minify_css: bool,
-    pub minify_css_level_1: bool,
-    pub minify_css_level_2: bool,
-    pub minify_css_level_3: bool,
-    pub minify_js: bool,
-    pub remove_bangs: bool,
-    pub remove_processing_instructions: bool,
+    pub do_not_minify_doctype: Option<bool>,
+    pub ensure_spec_compliant_unquoted_attribute_values: Option<bool>,
+    pub keep_closing_tags: Option<bool>,
+    pub keep_html_and_head_opening_tags: Option<bool>,
+    pub keep_spaces_between_attributes: Option<bool>,
+    pub keep_comments: Option<bool>,
+    pub minify_css: Option<bool>,
+    pub minify_css_level_1: Option<bool>,
+    pub minify_css_level_2: Option<bool>,
+    pub minify_css_level_3: Option<bool>,
+    pub minify_js: Option<bool>,
+    pub remove_bangs: Option<bool>,
+    pub remove_processing_instructions: Option<bool>,
 }
 
 #[wasm_bindgen]
 pub fn minify(code: &[u8], config: &Config) -> Vec<u8> {
     let cfg = minify_html::Cfg {
-        do_not_minify_doctype: config.do_not_minify_doctype,
+        do_not_minify_doctype: config.do_not_minify_doctype.unwrap_or(false),
         ensure_spec_compliant_unquoted_attribute_values: config
-            .ensure_spec_compliant_unquoted_attribute_values,
-        keep_closing_tags: config.keep_closing_tags,
-        keep_html_and_head_opening_tags: config.keep_html_and_head_opening_tags,
-        keep_spaces_between_attributes: config.keep_spaces_between_attributes,
-        keep_comments: config.keep_comments,
-        minify_css: config.minify_css,
-        minify_css_level_1: config.minify_css_level_1,
-        minify_css_level_2: config.minify_css_level_2,
-        minify_css_level_3: config.minify_css_level_3,
-        minify_js: config.minify_js,
-        remove_bangs: config.remove_bangs,
-        remove_processing_instructions: config.remove_processing_instructions,
+            .ensure_spec_compliant_unquoted_attribute_values
+            .unwrap_or(false),
+        keep_closing_tags: config.keep_closing_tags.unwrap_or(false),
+        keep_html_and_head_opening_tags: config.keep_html_and_head_opening_tags.unwrap_or(false),
+        keep_spaces_between_attributes: config.keep_spaces_between_attributes.unwrap_or(false),
+        keep_comments: config.keep_comments.unwrap_or(false),
+        minify_css: config.minify_css.unwrap_or(false),
+        minify_css_level_1: config.minify_css_level_1.unwrap_or(false),
+        minify_css_level_2: config.minify_css_level_2.unwrap_or(false),
+        minify_css_level_3: config.minify_css_level_3.unwrap_or(false),
+        minify_js: config.minify_js.unwrap_or(false),
+        remove_bangs: config.remove_bangs.unwrap_or(false),
+        remove_processing_instructions: config.remove_processing_instructions.unwrap_or(false),
     };
     minify_html::minify(code, &cfg)
 }
